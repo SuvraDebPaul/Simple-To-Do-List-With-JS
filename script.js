e258d3dfe7912f355a5db6aa9bf2e25d3e870e8a
Suvra Deb Paul <suvra.deb.paul@gmail.com>
2025-08-06 21:40:48 +0600

    Adding Functionality of JavaScript

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const listContainer = document.getElementById("listContainer");
 const button = document.getElementById("button");
 
 button.addEventListener("click", addTask);
+listContainer.addEventListener("click", checkedRemove);
 
-function addTask(e) {
+function addTask() {
   if (inputBox.value === "") {
     alert("Please Insert a Valid Task");
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+}
+
+function checkedRemove(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
   }
 }
